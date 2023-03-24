cb19d90e4b19fdf903694f091e5872d1868656ab
savomaaa <savomaaa@jyu.fi>
2023-03-24 22:36:42 +0200

    laprecordtrackeriin lisätty Kilparadat-luokka

--- src/laprecordtracker/LapRecordTracker.java
+++ src/laprecordtracker/LapRecordTracker.java
@@ -3,6 +3,8 @@
  */
 package laprecordtracker;
 
+import java.util.List;
+
 /**
  * CRC sisällöt tähän
  * @author Matti Savolainen
@@ -11,7 +13,8 @@ package laprecordtracker;
  */
 public class LapRecordTracker {
     
-    Kierrosajat kierrosajat = new Kierrosajat();
+    private final Kierrosajat kierrosajat = new Kierrosajat();
+    private final Kilparadat kilparadat = new Kilparadat();
     
     
     /**
@@ -21,6 +24,15 @@ public class LapRecordTracker {
      */
     public void lisaa(Kierrosaika kierrosaika) throws SailoException {
         kierrosajat.lisaa(kierrosaika);
+    }
+    
+    
+    /**
+     * Lisätään uusi kilparata laprecordtrackeriin
+     * @param kil lisättävä kilparata
+     */
+    public void lisaa(Kilparata kil) {
+        kilparadat.lisaa(kil);
     }
     
     
@@ -39,6 +51,42 @@ public class LapRecordTracker {
      */
     public Kierrosaika annaKierrosaika(int i) {
         return kierrosajat.anna(i);
+    }
+    
+    
+    /**
+     * Haetaan kierrosajan kilparata
+     * @param kierrosaika kierrosaika jolle kilparata haetaan
+     * @return tietorakenne jossa viitteet löydettyihin kilparatoihin
+     * @example
+     * <pre name="test">
+     * #import java.util.*;
+     * 
+     * LapRecordTracker laprecordtracker = new LapRecordTracker();
+     * Kierrosaika zonda1 = new Kierrosaika(), zonda2 = new Kierrosaika(), zonda3 = new Kierrosaika();
+     * zonda1.rekisteroi(); zonda2.rekisteroi(); zonda3.rekisteroi();
+     * int id1 = zonda1.getTunnusNro();
+     * int id2 = zonda2.getTunnusNro();
+     * Kilparata kilpa11 = new Kilparata(id1); laprecordtracker.lisaa(kilpa11);
+     * Kilparata kilpa12 = new Kilparata(id1); laprecordtracker.lisaa(kilpa12);
+     * Kilparata kilpa21 = new Kilparata(id2); laprecordtracker.lisaa(kilpa21);
+     * Kilparata kilpa22 = new Kilparata(id2); laprecordtracker.lisaa(kilpa22);
+     * Kilparata kilpa23 = new Kilparata(id2); laprecordtracker.lisaa(kilpa23);
+     * 
+     * List<Kilparata> loytyneet;
+     * loytyneet = laprecordtracker.annaKilparadat(zonda3);
+     * loytyneet.size() === 0;
+     * loytyneet = laprecordtracker.annaKilparadat(zonda1);
+     * loytyneet.size() === 2;
+     * loytyneet.get(0) == kilpa11 === true;
+     * loytyneet.get(1) == kilpa12 === true;
+     * loytyneet = laprecordtracker.annaKilparadat(zonda2);
+     * loytyneet.size() === 3; 
+     * loytyneet.get(0) == kilpa21 === true;
+     * </pre>
+     */
+    public List<Kilparata> annaKilparadat(Kierrosaika kierrosaika){
+        return kilparadat.annaKilparadat(kierrosaika.getTunnusNro());
     }
     
 
